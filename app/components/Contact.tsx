d774965ad5d42265295ff23eb2a5d01eee2c0cc1
AiDinaAgustin <aidinaagustin2@gmail.com>
2025-11-12 11:54:17 +0700

    Remove unused Image import from Contact component

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState } from 'react'
-import Image from 'next/image'
 import Link from 'next/link'
 import { Mail, Phone, Linkedin, Github, FileDown } from "lucide-react"
 
